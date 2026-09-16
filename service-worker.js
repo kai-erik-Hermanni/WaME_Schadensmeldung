0b85f2572c048e1a307641bb175a629b485ed74b
kai-erik-Hermanni <kai-erik@web.de>
2026-09-16 09:24:13 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,11 @@
-const CACHE='schadenbericht-wasserversorgung-v1-86';
+const CACHE='schadenbericht-wasserversorgung-v1-87-force-refresh';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
+  self.clients.claim()
+])));
+self.addEventListener('fetch',e=>e.respondWith(
+  fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r})
+  .catch(()=>caches.match(e.request))
+));
