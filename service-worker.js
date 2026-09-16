6bbd8b459a94779555971135060dd7042b93bbae
kai-erik-Hermanni <kai-erik@web.de>
2026-09-16 09:21:16 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='schadenbericht-wasserversorgung-v1-85';
+const CACHE='schadenbericht-wasserversorgung-v1-86';
 const ASSETS=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
